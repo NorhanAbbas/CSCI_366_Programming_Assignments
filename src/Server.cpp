bc4748d620746a7c7eadb543725f7854d9635715
NorhanAbbas <48738927+NorhanAbbas@users.noreply.github.com>
2020-03-13 16:56:51 -0600

    Add files via upload

--- src/Server.cpp
+++ src/Server.cpp
@@ -32,13 +32,124 @@ int get_file_length(ifstream *file){
 void Server::initialize(unsigned int board_size,
                         string p1_setup_board,
                         string p2_setup_board){
+
+   if (board_size != BOARD_SIZE) {
+
+        throw ServerException("the board size is not right");
+    }
+
+   else if (p1_setup_board == "" && p2_setup_board == ""){
+       throw ServerException("one of the files does NOT exist");
+   }
+    else{
+
+        this->board_size = board_size;
+    }
 }
 
 
 int Server::evaluate_shot(unsigned int player, unsigned int x, unsigned int y) {
+
+    vector<string> lines(board_size,"");
+
+    ifstream myfile;
+
+    // check if the coordinates are within the possible limits
+    if (x > board_size-1 || x < 0 || y < 0 || y > board_size-1){
+        return OUT_OF_BOUNDS;
+    }
+
+    if (player > MAX_PLAYERS || player < 1){
+        throw ServerException("invalid player number");
+    }
+
+    // compare the player's board against his/her opponent's
+
+    if(player==2){
+        player=1;
+    }
+    else{
+        player=2;
+    }
+
+    string file_name = "player_"+ to_string(player) +".setup_board.txt";
+
+    myfile.open(file_name);
+
+    string each_line;
+    string *pointer;
+    int i=0, k = 0;
+    char target;
+
+    while(!myfile.eof()) {
+
+        while(getline(myfile,each_line)){
+            lines[i] = each_line;
+
+            i++;
+        }
+    }
+    myfile.close();
+
+    target = lines[y].at(x);
+
+    // this target should be
+
+    if (target == 'C' || target == 'B' || target == 'R' || target == 'S' || target == 'D') {
+        return HIT;
+    }
+    else {
+        return MISS;
+    }
+
 }
 
-
 int Server::process_shot(unsigned int player) {
-   return NO_SHOT_FILE;
+    // https://stackoverflow.com/questions/47297648/using-cereal-to-deserialize-a-json-string
+    int x;
+    int y;
+    int shot_result;
+    int result;
+
+    std::ifstream myfile;
+    //ifstream myfile;
+    ofstream outfile;
+
+    if (player > MAX_PLAYERS || player <= 0) {
+        throw ServerException("invalid player number");
+    }
+
+
+    string file_nameOut = "player_" + to_string(player) + ".result.json";
+    string file_nameIn = "player_" + to_string(player) + ".shot.json";
+
+    myfile.open(file_nameIn);
+
+
+    if (!myfile) {
+        return NO_SHOT_FILE;
+    }
+
+    // deserialize; convert JSON string to custom
+    //pass the stream sting object into the input archive
+    cereal::JSONInputArchive archive_in(myfile);
+
+    // use input archive to extract data to my program
+    archive_in(x, y);
+    cout << x << endl;
+    cout << y << endl;
+
+    remove((file_nameIn).c_str());
+
+
+
+    result = evaluate_shot(player, x, y);
+    myfile.close();
+
+    outfile.open(file_nameOut);
+
+    cereal::JSONOutputArchive archive_out(outfile);
+    archive_out(CEREAL_NVP(result));
+          return SHOT_FILE_PROCESSED;
+
 }
